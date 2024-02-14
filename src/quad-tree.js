6413d4fef7801ffbb62312c49cf5e486db5c574e
caisilus <serangor@gmail.com>
2024-02-14 12:13:19 +0300

    Format everything with prettier

--- src/quad-tree.js
+++ src/quad-tree.js
@@ -1,46 +1,45 @@
-import Rectangle from './rectangle'
+import Rectangle from "./rectangle";
 
 export default class QuadTree {
-    constructor(boundary, capacity = 4) {
-        if (!boundary) {
-            throw TypeError('boundary is null or undefined')
-        }
-
-        if (!(boundary instanceof Rectangle)) {
-            throw TypeError('boundary should be a Rectangle')
-        }
-
-        this._points = []
-        this._boundary = boundary
-        this._capacity = capacity
-        this._hasChildren = false
-        this._children = []
+  constructor(boundary, capacity = 4) {
+    if (!boundary) {
+      throw TypeError("boundary is null or undefined");
     }
 
-    insert(point) {
-        return true
+    if (!(boundary instanceof Rectangle)) {
+      throw TypeError("boundary should be a Rectangle");
     }
 
-    get length() {
-        let count = this._points.length
-        if (this._hasChildren) {
-            // handle childrens somehow
-        }
-        return count
-    }
+    this._points = [];
+    this._boundary = boundary;
+    this._capacity = capacity;
+    this._hasChildren = false;
+    this._children = [];
+  }
 
-    queryRange(rect, found = []) {
-        return found
-    }
+  insert(point) {
+    return true;
+  }
 
-    _subdivide() {
+  get length() {
+    let count = this._points.length;
+    if (this._hasChildren) {
+      // handle childrens somehow
     }
+    return count;
+  }
 
-    clear() {
-        // clear _points and _children arrays
-        // see https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
-        this._points = []
-        this._children = []
-        this._hasChildren = false
-    }
+  queryRange(rect, found = []) {
+    return found;
+  }
+
+  _subdivide() {}
+
+  clear() {
+    // clear _points and _children arrays
+    // see https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
+    this._points = [];
+    this._children = [];
+    this._hasChildren = false;
+  }
 }
